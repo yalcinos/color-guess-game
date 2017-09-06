9a4892e12e978444e17b1046d9f16efe47d35ccd
yalcintatar <yalcintatar41@gmail.com>
2017-09-06 23:28:24 +0300

    Added Random Color function and some js improvement.

--- js/colorGuess.js
+++ js/colorGuess.js
@@ -1,15 +1,9 @@
-var colors=[
-"rgb(255, 0 , 0)",
-"rgb(255, 255, 0)",
-"rgb(0, 255, 0)",
-"rgb(0, 255, 255)",
-"rgb(0, 0, 255)",
-"rgb(255, 0, 255)", 
-];
 
+var colors=randomColors(6);
 var squares=document.querySelectorAll(".square");
-var colorpicked=colors[3];
+var colorpicked=pickColorAction();
 var colorDisplay=document.getElementById("colorrgb");
+var resultDisplay=document.getElementById("resultMessage");
 
 colorDisplay.textContent=colorpicked;
 for(var i=0; i<squares.length; i++){
@@ -17,12 +11,39 @@ for(var i=0; i<squares.length; i++){
 
 	squares[i].addEventListener("click", function(){
 		var clickedColor=this.style.backgroundColor;
+		
 		if(clickedColor===colorpicked){
-			alert("Correct!");
+			resultDisplay.textContent="Correct!";
+			changeColorOfSquares();
 		}
 		else
 		{
-			alert("Wrong!");
+			this.style.backgroundColor="#232323";
+			resultDisplay.textContent="False!"; 
 		}
 	});
+}
+
+function changeColorOfSquares() {
+	for(var i=0; i<squares.length; i++){
+		squares[i].style.backgroundColor=colorpicked;
+	}
+}
+function pickColorAction(){
+	var randomnumber=Math.floor(Math.random()*colors.length);
+	return colors[randomnumber];
+}
+function generateColor(){
+	var r=Math.floor(Math.random()*255+1);
+	var g=Math.floor(Math.random()*255+1);
+	var b=Math.floor(Math.random()*255+1);
+	return "rgb("+r+", "+g+", "+b+")";
+}
+function randomColors(number)
+{
+	var colorArray=[];
+	for(var i=0; i<number;i++){
+		colorArray.push(generateColor());	
+	}
+	return colorArray;
 }
